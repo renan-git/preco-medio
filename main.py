d85fbd826efbee81a41bb7e4fd9f93e47bbec418
Renan <renan.cruz10@gmail.com>
2026-06-03 18:11:28 -0300

    Estrutura inicial, histórico e carteira atual

---
+++ main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+compras = pd.read_csv('acoes/BBAS3/compras.csv')
+vendas = pd.read_csv('acoes/BBAS3/vendas.csv')
+
+compras['tipo'] = 'COMPRA'
+vendas['tipo'] = 'VENDA'
+
+operacoes = pd.concat([compras, vendas])
+
+operacoes['data'] = pd.to_datetime(operacoes['data'])
+operacoes = operacoes.sort_values('data')
+
+carteira = {}
+historico = {}
+
+for _, operacao in operacoes.iterrows():
+    ativo = operacao['ativo']
+    tipo = operacao['tipo']
+    ano = operacao['data'].year
+    quantidade = operacao['quantidade']
+    preco = operacao['preco_unidade']
+    taxa = operacao['taxa']
+    if ativo not in carteira:
+        carteira[ativo] = {
+            'quantidade': 0,
+            'custo_total': 0,
+            'preco_medio': 0
+        }
+    dados = carteira[ativo]
+    if tipo == 'COMPRA':
+        custo_compra = (quantidade * preco) + taxa
+        dados['custo_total'] += custo_compra
+        dados['quantidade'] += quantidade
+        dados['preco_medio'] = round(dados['custo_total'] / dados['quantidade'], 2)
+    if tipo == 'VENDA':
+        dados['quantidade'] -= quantidade
+        if dados['quantidade'] == 0:
+            dados['custo_total'] = 0
+            dados['preco_medio'] = 0
+        else:
+            dados['custo_total'] = dados['quantidade'] * dados['preco_medio']
+    if ativo not in historico:
+        historico[ativo] = {}
+    historico[ativo][ano] = {
+        'quantidade': dados['quantidade'],
+        'custo_total': dados['custo_total'],
+        'preco_medio': dados['preco_medio']
+    }
+
+
+for ativo in carteira.values():
+    ativo['custo_total'] = round(ativo['custo_total'], 2)
+    ativo['preco_medio'] = round(ativo['preco_medio'], 2)
+
+
+for ativo, anos in historico.items():
+    for ano, dados in anos.items():
+        dados['custo_total'] = round(dados['custo_total'],2)
+        dados['preco_medio'] = round(dados['preco_medio'],2)
+
+print(carteira)
+
+print(historico)
